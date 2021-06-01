2a1d074c4f2099610af2bec480f399676790aa59
Oleksandr gardener <boltbright525@gmail.com>
2021-06-01 11:31:13 -0700

    button has a selected "state" now

--- components/basic/Button.js
+++ components/basic/Button.js
@@ -1,8 +1,8 @@
-export default function Button({onClick,onFocus,role,className,text,children,ring = false}){
+export default function Button({onClick,onFocus,role,className,text,children,selected = false}){
   return <button
           onClick={onClick}
           onFocus={onFocus}
           role={role}
-          className={`${ring ? 'ring ring-md' : ''} my-2 flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:text-brand-blue bg-brand-blue hover:bg-brand-blue-light`}
+          className={`${selected ? 'text-brand-blue bg-brand-blue-light' : 'bg-brand-blue text-white'} my-2 flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium  hover:text-brand-blue hover:bg-brand-blue-light`}
           >{text ? text : children}</button>
 }
